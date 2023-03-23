e08a9e37d65a0fa01d265d68d2bac29032cdc98a
Logan Klier <logan@elevator3.com>
2023-03-23 10:16:59 -0500

    fix: missing express

--- testmongo.js
+++ testmongo.js
@@ -1,23 +1,35 @@
 const { MongoClient } = require("mongodb");
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.listen(port);
+console.log("Server started at http://localhost:" + port);
+
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://classuser:cmps415class@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://classuser:cmps415class@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 
-async function run() {
-  try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+app.get("/", function (req, res) {
+  async function run() {
+    try {
+      const database = client.db("ckmdb");
+      const movies = database.collection("cmps415");
 
-    // Query for a part that has partID '12345'
-    const query = { partID: '12345' };
-    const part = await parts.findOne(query);
+      // Query for a movie that has the title 'Back to the Future'
+      const query = { partID: "12345" };
+      const movie = await movies.findOne(query);
 
-    console.log(part);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+      console.log(movie);
+      res.send(movie);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
   }
-}
-run().catch(console.dir);
+  run().catch(console.dir);
+});
